42c4fbe15b00b10e39235cff871ce21f74a2c53e
AWKRID <minzoo202738@gmail.com>
2024-05-01 14:45:01 +0900

    :sparkles: Lecture 1: Create a Spring Boot Project

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,9 +2,24 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DemoApplication
+
+
+// Tell Spring that MessageController is a REST controller,
+@RestController
+class MessageController {
+    // Mark the function below that implement the endpoints corresponding to HTTP GET calls
+    @GetMapping("/")
+    // @RequestParam: Indicates that method parameter(name) should be bound to the web request parameter
+    // http://localhost:8080/?name=<your-name>
+    fun index(@RequestParam name: String) = "Hello $name"
+}
+
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
