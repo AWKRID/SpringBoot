30da9436519bcf1d39f87ec66906bbf2dfc33bce
AWKRID <minzoo202738@gmail.com>
2024-05-01 17:54:31 +0900

    :sparkles: Lecture 4: Use Spring Data CrudRepository

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,39 +2,58 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.*
 import org.springframework.jdbc.core.query
 import java.util.*
+import kotlin.jvm.optionals.toList
 
 @SpringBootApplication
 class DemoApplication
 
-data class Message(val id: String?, val text: String)
+interface MessageRepository : CrudRepository<Message, String>
+
+@Table("MESSAGES")
+data class Message(@Id val id: String?, val text: String)
+
 
 
 // to implement the database access login within service layer, use @Service annotation
+//@Service
+//class MessageService(val db: JdbcTemplate) {
+//    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+//        Message(response.getString("id"), response.getString("text"))
+//    }
+//
+//    fun findMessageById(id: String): List<Message> =
+//        db.query("select * from messages where id = ?", id) { response, _ ->
+//            Message(response.getString("id"), response.getString("text"))
+//        }
+//
+//    fun save(message: Message) {
+//        val id = message.id ?: UUID.randomUUID().toString()
+//        db.update(
+//            "insert into messages values (? , ?)", id, message.text
+//        )
+//
+//    }
+//}
 @Service
-class MessageService(val db: JdbcTemplate) {
-    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
-        Message(response.getString("id"), response.getString("text"))
-    }
+class MessageService(val db: MessageRepository) {
+    fun findMessages(): List<Message> = db.findAll().toList()
 
-    fun findMessageById(id: String): List<Message> =
-        db.query("select * from messages where id = ?", id) { response, _ ->
-            Message(response.getString("id"), response.getString("text"))
-        }
+    fun findMessageById(id: String): List<Message> = db.findById(id).toList()
 
     fun save(message: Message) {
-        val id = message.id ?: UUID.randomUUID().toString()
-        db.update(
-            "insert into messages values (? , ?)", id, message.text
-        )
-
+        db.save(message)
     }
+    fun <T: Any> Optional<out T>.toList(): List<T> =
+        if(isPresent) listOf(get()) else emptyList()
 }
-
 
 // Tell Spring that MessageController is a REST controller,
 @RestController
